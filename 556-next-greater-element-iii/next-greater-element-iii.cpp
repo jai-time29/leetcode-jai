c4b38b6ef32159fef1cd815488a6664aca32ef31
Dhatrik Jaikritha <krithajai@gmail.com>
2026-05-11 14:56:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (99.40%) - LeetSync

---
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+
+        string s = to_string(n);
+
+        int i = s.size() - 2;
+
+        // Find pivot
+        while (i >= 0 && s[i] >= s[i + 1]) {
+            i--;
+        }
+
+        if (i < 0) return -1;
+
+        // Find next greater digit
+        int j = s.size() - 1;
+
+        while (s[j] <= s[i]) {
+            j--;
+        }
+
+        swap(s[i], s[j]);
+
+        // Make suffix smallest
+        reverse(s.begin() + i + 1, s.end());
+
+        long long ans = stoll(s);
+
+        return (ans > INT_MAX) ? -1 : ans;
+    }
+};
