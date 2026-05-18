895459230e94cbe9709114e97edad84ae626d4b7
Dhatrik Jaikritha <krithajai@gmail.com>
2026-05-18 20:50:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (99.59%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        int l = 0;
+        int r = s.size() - 1;
+
+        while (l < r) {
+
+            // skip non-alphanumeric
+            while (l < r && !isalnum(s[l])) l++;
+
+            while (l < r && !isalnum(s[r])) r--;
+
+            // compare lowercase chars
+            if (tolower(s[l]) != tolower(s[r]))
+                return false;
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+};
