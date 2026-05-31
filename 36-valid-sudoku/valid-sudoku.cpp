bd34b3311d2c7e30ef0287d64fadba7391b65552
Dhatrik Jaikritha <krithajai@gmail.com>
2026-05-31 13:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (80.75%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*  class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
 
@@ -28,6 +28,45 @@ public:
                 rows[i].insert(ch);
                 cols[j].insert(ch);
                 boxes[box].insert(ch);
+            }
+        }
+
+        return true;
+    }
+}; */
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        vector<int> rows(9, 0);
+        vector<int> cols(9, 0);
+        vector<int> boxes(9, 0);
+
+        for(int i = 0; i < 9; i++) {
+
+            for(int j = 0; j < 9; j++) {
+
+                if(board[i][j] == '.')
+                    continue;
+
+                int num = board[i][j] - '0';
+
+                int mask = 1 << num;
+
+                int box = (i / 3) * 3 + (j / 3);
+
+                // already exists
+                if((rows[i] & mask) ||
+                   (cols[j] & mask) ||
+                   (boxes[box] & mask)) {
+
+                    return false;
+                }
+
+                // mark present
+                rows[i] |= mask;
+                cols[j] |= mask;
+                boxes[box] |= mask;
             }
         }
 
