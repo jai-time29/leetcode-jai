766cfb8bdd535aeb520c98cf7985ef7a4e72c62b
Dhatrik Jaikritha <krithajai@gmail.com>
2026-06-01 20:30:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (98.94%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
 
     int hammingWeight(uint32_t n) {
@@ -10,6 +10,23 @@ public:
             n = n & (n - 1);
 
             count++;
+        }
+
+        return count;
+    }
+};  */
+class Solution {
+public:
+
+    int hammingWeight(uint32_t n) {
+
+        int count = 0;
+
+        while(n) {
+
+            count += (n & 1);
+
+            n >>= 1;
         }
 
         return count;
