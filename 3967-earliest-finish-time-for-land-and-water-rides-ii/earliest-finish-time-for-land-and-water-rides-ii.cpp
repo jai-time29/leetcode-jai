4f507d38cdbf2df497b2c2095907698fd3589c78
Dhatrik Jaikritha <krithajai@gmail.com>
2026-06-03 13:07:38 +0530

    Time: 115 ms (22.02%) | Memory: 262 MB (19.27%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -1,61 +1,43 @@
 class Solution {
 public:
 
-    long long getBest(vector<int>& startA, vector<int>& durA,
-                      vector<int>& startB, vector<int>& durB) {
+    struct Ride {
+        int start;
+        int dur;
+    };
 
-        int m = startB.size();
+    long long solve(const vector<int>& startA,
+                    const vector<int>& durA,
+                    const vector<int>& startsB,
+                    const vector<int>& prefDurB,
+                    const vector<int>& suffFinishB) {
 
-        vector<pair<int,int>> b(m);
-
-        for(int i = 0; i < m; i++) {
-            b[i] = {startB[i], durB[i]};
-        }
-
-        sort(b.begin(), b.end());
-
-        vector<int> starts(m);
-        vector<long long> pref(m), suff(m);
-
-        for(int i = 0; i < m; i++) {
-            starts[i] = b[i].first;
-        }
-
-        pref[0] = b[0].second;
-
-        for(int i = 1; i < m; i++) {
-            pref[i] = min(pref[i - 1], (long long)b[i].second);
-        }
-
-        suff[m - 1] = (long long)b[m - 1].first + b[m - 1].second;
-
-        for(int i = m - 2; i >= 0; i--) {
-            suff[i] = min(
-                suff[i + 1],
-                (long long)b[i].first + b[i].second
-            );
-        }
-
+        int m = startsB.size();
         long long ans = LLONG_MAX;
 
         for(int i = 0; i < startA.size(); i++) {
 
-            long long finishA =
-                (long long)startA[i] + durA[i];
+            long long finishA = (long long)startA[i] + durA[i];
 
             int pos = upper_bound(
-                starts.begin(),
-                starts.end(),
+                startsB.begin(),
+                startsB.end(),
                 finishA
-            ) - starts.begin();
+            ) - startsB.begin();
 
-            if(pos)
-                ans = min(ans,
-                          finishA + pref[pos - 1]);
+            if(pos > 0) {
+                ans = min(
+                    ans,
+                    finishA + (long long)prefDurB[pos - 1]
+                );
+            }
 
-            if(pos < m)
-                ans = min(ans,
-                          suff[pos]);
+            if(pos < m) {
+                ans = min(
+                    ans,
+                    (long long)suffFinishB[pos]
+                );
+            }
         }
 
         return ans;
@@ -66,12 +48,98 @@ public:
                            vector<int>& waterStartTime,
                            vector<int>& waterDuration) {
 
-        return (int)min(
-            getBest(landStartTime, landDuration,
-                    waterStartTime, waterDuration),
+        int n = landStartTime.size();
+        int m = waterStartTime.size();
 
-            getBest(waterStartTime, waterDuration,
-                    landStartTime, landDuration)
+        vector<Ride> land(n), water(m);
+
+        for(int i = 0; i < n; i++)
+            land[i] = {landStartTime[i], landDuration[i]};
+
+        for(int i = 0; i < m; i++)
+            water[i] = {waterStartTime[i], waterDuration[i]};
+
+        sort(land.begin(), land.end(),
+             [](const Ride& a, const Ride& b) {
+                 return a.start < b.start;
+             });
+
+        sort(water.begin(), water.end(),
+             [](const Ride& a, const Ride& b) {
+                 return a.start < b.start;
+             });
+
+        vector<int> landStarts(n);
+        vector<int> waterStarts(m);
+
+        vector<int> landPrefDur(n);
+        vector<int> waterPrefDur(m);
+
+        vector<int> landSuffFinish(n);
+        vector<int> waterSuffFinish(m);
+
+        for(int i = 0; i < n; i++)
+            landStarts[i] = land[i].start;
+
+        for(int i = 0; i < m; i++)
+            waterStarts[i] = water[i].start;
+
+        landPrefDur[0] = land[0].dur;
+        for(int i = 1; i < n; i++)
+            landPrefDur[i] =
+                min(landPrefDur[i - 1], land[i].dur);
+
+        waterPrefDur[0] = water[0].dur;
+        for(int i = 1; i < m; i++)
+            waterPrefDur[i] =
+                min(waterPrefDur[i - 1], water[i].dur);
+
+        landSuffFinish[n - 1] =
+            land[n - 1].start + land[n - 1].dur;
+
+        for(int i = n - 2; i >= 0; i--) {
+            landSuffFinish[i] = min(
+                landSuffFinish[i + 1],
+                land[i].start + land[i].dur
+            );
+        }
+
+        waterSuffFinish[m - 1] =
+            water[m - 1].start + water[m - 1].dur;
+
+        for(int i = m - 2; i >= 0; i--) {
+            waterSuffFinish[i] = min(
+                waterSuffFinish[i + 1],
+                water[i].start + water[i].dur
+            );
+        }
+
+        long long ans = LLONG_MAX;
+
+        // Land -> Water
+        ans = min(
+            ans,
+            solve(
+                landStartTime,
+                landDuration,
+                waterStarts,
+                waterPrefDur,
+                waterSuffFinish
+            )
         );
+
+        // Water -> Land
+        ans = min(
+            ans,
+            solve(
+                waterStartTime,
+                waterDuration,
+                landStarts,
+                landPrefDur,
+                landSuffFinish
+            )
+        );
+
+        return (int)ans;
     }
 };
