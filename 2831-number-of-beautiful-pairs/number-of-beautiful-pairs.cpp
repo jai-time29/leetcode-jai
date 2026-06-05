91728feb4c3751e8172ced7b9b7fc680aad61730
Dhatrik Jaikritha <krithajai@gmail.com>
2026-06-05 13:26:51 +0530

    Time: 11 ms (60.70%) | Memory: 73.2 MB (33.55%) - LeetSync

--- 2831-number-of-beautiful-pairs/number-of-beautiful-pairs.cpp
+++ 2831-number-of-beautiful-pairs/number-of-beautiful-pairs.cpp
@@ -14,7 +14,7 @@ public:
         return x;
     }
 
-    int countBeautifulPairs(vector<int>& nums) {
+    /*int countBeautifulPairs(vector<int>& nums) {
         int n = nums.size();
         int ans = 0;
 
@@ -25,6 +25,22 @@ public:
                 int last = nums[j] % 10;
 
                 if(gcd(first, last) == 1)
+                    ans++;
+            }
+        }
+
+        return ans;
+    }
+    */
+    int countBeautifulPairs(vector<int>& nums) {
+        int ans = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int first = nums[i];
+            while(first >= 10) first /= 10;
+
+            for(int j = i + 1; j < nums.size(); j++) {
+                if(__gcd(first, nums[j] % 10) == 1)
                     ans++;
             }
         }
