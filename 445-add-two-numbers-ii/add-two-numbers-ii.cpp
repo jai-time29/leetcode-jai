7407aed635626446b0fb1f0b18df29ee67b38bd8
Dhatrik Jaikritha <krithajai@gmail.com>
2026-06-15 16:14:30 +0530

    Time: 0 ms (100.00%) | Memory: 76.8 MB (84.18%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -8,7 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
 
@@ -60,5 +60,77 @@ public:
 
 
         return head;
+    }
+}; */
+class Solution {
+public:
+
+    ListNode* reverse(ListNode* head){
+
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while(curr){
+
+            ListNode* next = curr->next;
+
+            curr->next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+
+    ListNode* add(ListNode* l1, ListNode* l2){
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+
+        int carry = 0;
+
+        while(l1 || l2 || carry){
+
+            int sum = carry;
+
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if(l2){
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+
+            carry = sum / 10;
+
+            temp->next = new ListNode(sum % 10);
+            temp = temp->next;
+        }
+
+        return dummy->next;
+    }
+
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        // reverse input lists
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+
+        // add normally
+        ListNode* ans = add(l1, l2);
+
+
+        // reverse result to restore most significant digit first
+        ans = reverse(ans);
+
+
+        return ans;
     }
 };
