82e622ccb36a3ee3aa49106899662ccc232b01f1
Dhatrik Jaikritha <krithajai@gmail.com>
2026-07-03 01:06:04 +0530

    Time: 1 ms (22.99%) | Memory: 45.1 MB (88.55%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -14,3 +14,25 @@ public:
         return mx - mn;
     }
 };
+/*
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+
+        int curMax = nums[0], maxSum = nums[0];
+        int curMin = nums[0], minSum = nums[0];
+
+        for(int i = 1; i < nums.size(); i++) {
+
+            // Normal Kadane
+            curMax = max(nums[i], curMax + nums[i]);
+            maxSum = max(maxSum, curMax);
+
+            // Minimum Kadane
+            curMin = min(nums[i], curMin + nums[i]);
+            minSum = min(minSum, curMin);
+        }
+
+        return max(maxSum, -minSum);
+    }
+}; */
