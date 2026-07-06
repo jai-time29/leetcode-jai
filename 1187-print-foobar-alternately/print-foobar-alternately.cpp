c278daba31b4d595ac7942065a8dc5f86287c04a
Dhatrik Jaikritha <krithajai@gmail.com>
2026-07-06 22:32:20 +0530

    Time: 9 ms (53.25%) | Memory: 11.3 MB (43.16%) - LeetSync

---
+++ 1187-print-foobar-alternately/print-foobar-alternately.cpp
@@ -0,0 +1,35 @@
+#include <semaphore>
+
+class FooBar {
+private:
+    int n;
+    binary_semaphore fooSem{1};
+    binary_semaphore barSem{0};
+
+public:
+    FooBar(int n) {
+        this->n = n;
+    }
+
+    void foo(function<void()> printFoo) {
+
+        for (int i = 0; i < n; i++) {
+            fooSem.acquire();
+
+            printFoo();
+
+            barSem.release();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+
+        for (int i = 0; i < n; i++) {
+            barSem.acquire();
+
+            printBar();
+
+            fooSem.release();
+        }
+    }
+};
