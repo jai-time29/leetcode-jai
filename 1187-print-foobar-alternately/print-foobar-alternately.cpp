5f7b2245fcaf33c3536b5b8a22b0d53ea5a535ca
Dhatrik Jaikritha <krithajai@gmail.com>
2026-07-06 22:36:02 +0530

    Time: 4 ms (84.81%) | Memory: 11.2 MB (93.16%) - LeetSync

--- 1187-print-foobar-alternately/print-foobar-alternately.cpp
+++ 1187-print-foobar-alternately/print-foobar-alternately.cpp
@@ -33,3 +33,52 @@ public:
         }
     }
 };
+/*
+class FooBar {
+private:
+    int n;
+    mutex mtx;
+    condition_variable cv;
+    bool fooTurn = true;
+
+public:
+    FooBar(int n) {
+        this->n = n;
+    }
+
+    void foo(function<void()> printFoo) {
+
+        for(int i=0;i<n;i++) {
+
+            unique_lock<mutex> lock(mtx);
+
+            cv.wait(lock,[&]{
+                return fooTurn;
+            });
+
+            printFoo();
+
+            fooTurn = false;
+
+            cv.notify_one();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+
+        for(int i=0;i<n;i++) {
+
+            unique_lock<mutex> lock(mtx);
+
+            cv.wait(lock,[&]{
+                return !fooTurn;
+            });
+
+            printBar();
+
+            fooTurn = true;
+
+            cv.notify_one();
+        }
+    }
+}; */
