ba041f3cc66f953b6e8ba7b2b3f36769211ee9cd
Dhatrik Jaikritha <krithajai@gmail.com>
2026-07-16 14:18:26 +0530

    Time: 132 ms (94.96%) | Memory: 258.9 MB (71.71%) - LeetSync

--- 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
+++ 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -6,21 +6,25 @@ public:
         if(n1!=n2)return -1;
         int n=n1;
 
-        unordered_map<int,int>cnt1,cnt2,cnt;
-        for(int i=0;i<n;i++){
-            cnt1[nums1[i]]++;
-            cnt2[nums2[i]]++;
-            cnt[nums1[i]]=1;
-            cnt[nums2[i]]=1;
-        }
-        int sumdiff=0,t=0;
-        for(auto it : cnt){
-            t=abs(cnt1[it.first]-cnt2[it.first]);
-            if(t%2!=0)return -1;
-            sumdiff+=t;
-            cout<<sumdiff;
-        }
-        return sumdiff/4;
+       
+        unordered_map<int,int> diff;
+diff.reserve(2 * n);
 
+for (int x : nums1)
+    diff[x]++;
+
+for (int x : nums2)
+    diff[x]--;
+
+long long sumdiff = 0;
+
+for (auto &[x, d] : diff) {
+    if (abs(d) & 1)
+        return -1;
+
+    sumdiff += abs(d);
+}
+
+return sumdiff / 4;
     }
 };
