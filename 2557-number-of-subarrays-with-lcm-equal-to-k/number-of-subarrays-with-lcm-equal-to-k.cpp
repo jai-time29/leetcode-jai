01ffe3a8da9902d0ca99a4f41ef10d24f2193da3
Dhatrik Jaikritha <krithajai@gmail.com>
2026-07-18 14:55:47 +0530

    Time: 15 ms (16.67%) | Memory: 13.3 MB (25.60%) - LeetSync

---
+++ 2557-number-of-subarrays-with-lcm-equal-to-k/number-of-subarrays-with-lcm-equal-to-k.cpp
@@ -0,0 +1,41 @@
+// class Solution {
+// public:
+//     int subarrayLCM(vector<int>& nums, int k) {
+//         int n=nums.size(),ans=0;
+//         long long tempgcd;
+//         for(int i=0;i<n;i++){
+//              tempgcd=1;
+//             for(int j=i;j<n;j++){
+//                 tempgcd=(tempgcd/gcd(tempgcd,1LL*nums[j]))*nums[j];
+//                 if(tempgcd==k)ans++;
+//                 // else if(tempgcd<k)break;
+//                 // else if(tempgcd%k!=0)break;
+//             }
+            
+//         }
+//         return ans;
+//     }
+// };
+class Solution {
+public:
+    int subarrayLCM(vector<int>& nums, int k) {
+        int n = nums.size(), ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            long long l = 1;
+
+            for (int j = i; j < n; j++) {
+                l = (l / gcd(l, 1LL * nums[j])) * nums[j];
+
+                if (l == k)
+                    ans++;
+                else if (l > k)
+                    break;
+                else if (k % l != 0)
+                    break;
+            }
+        }
+
+        return ans;
+    }
+};
