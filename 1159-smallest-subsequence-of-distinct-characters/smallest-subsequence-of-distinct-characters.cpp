5829447421a17890e3081211410944a89bb6f1e4
Dhatrik Jaikritha <krithajai@gmail.com>
2026-07-19 06:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (57.91%) - LeetSync

--- 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -2,33 +2,38 @@ class Solution {
 public:
     string smallestSubsequence(string s) {
         int n = s.length();
-        vector<int>freq(26,0);
-        for(int i=0;i<n;i++){
-               freq[s[i]-'a']++;
+
+        vector<int> freq(26, 0);
+        for (char c : s)
+            freq[c - 'a']++;
+
+        vector<int> vis(26, 0);
+        stack<char> st;
+
+        for (char c : s) {
+            freq[c - 'a']--;
+
+            if (vis[c - 'a'])
+                continue;
+
+            while (!st.empty() &&
+                   st.top() > c &&
+                   freq[st.top() - 'a'] > 0) {
+                vis[st.top() - 'a'] = 0;
+                st.pop();
+            }
+
+            st.push(c);
+            vis[c - 'a'] = 1;
         }
-       vector<int>vis(26,0);
-       stack<char>st;
-       char c;
-       for(int i=0;i<n;i++){
-          c=s[i];
-          freq[c-'a']--;
-          if(vis[c-'a'])continue;
-        while (!st.empty() &&
-       st.top() > s[i] &&
-       freq[st.top()-'a'] > 0)
-        {
-         vis[st.top()-'a'] = false;
-         st.pop();
-        }
-        st.push(c);
-        vis[c-'a']=1;
-       }
-        string ans ="";
-        while(!st.empty()){
-            ans+=st.top();
+
+        string ans;
+        while (!st.empty()) {
+            ans += st.top();
             st.pop();
         }
-        reverse(ans.begin(),ans.end());
+
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
