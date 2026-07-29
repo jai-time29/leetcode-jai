e7ba115f757031950b7b648598d20e076c447a56
Dhatrik Jaikritha <krithajai@gmail.com>
2026-07-29 13:11:30 +0530

    Time: 21 ms (69.56%) | Memory: 17.4 MB (36.23%) - LeetSync

---
+++ 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    static constexpr long long LIM = 1000000LL + 5;
+
+    long long comb(int n, int r) {
+        if (r < 0 || r > n) return 0;
+        r = min(r, n - r);
+
+        long long ans = 1;
+        for (int i = 1; i <= r; i++) {
+            ans = ans * (n - r + i) / i;
+            if (ans >= LIM) return LIM;
+        }
+        return ans;
+    }
+
+    long long countWays(vector<int>& cnt) {
+        int rem = 0;
+        for (int x : cnt) rem += x;
+
+        long long ways = 1;
+
+        for (int f : cnt) {
+            if (f == 0) continue;
+            ways *= comb(rem, f);
+            if (ways >= LIM) return LIM;
+            rem -= f;
+        }
+        return ways;
+    }
+
+    string smallestPalindrome(string s, int k) {
+        vector<int> freq(26);
+
+        for (char c : s) freq[c - 'a']++;
+
+        vector<int> half(26);
+        string mid = "";
+
+        int len = 0;
+
+        for (int i = 0; i < 26; i++) {
+            half[i] = freq[i] / 2;
+            len += half[i];
+            if (freq[i] & 1)
+                mid.push_back(char('a' + i));
+        }
+
+        if (countWays(half) < k)
+            return "";
+
+        string left;
+
+        for (int pos = 0; pos < len; pos++) {
+            for (int c = 0; c < 26; c++) {
+                if (half[c] == 0) continue;
+
+                half[c]--;
+
+                long long ways = countWays(half);
+
+                if (ways >= k) {
+                    left.push_back(char('a' + c));
+                    break;
+                }
+
+                k -= ways;
+                half[c]++;
+            }
+        }
+
+        string right = left;
+        reverse(right.begin(), right.end());
+
+        return left + mid + right;
+    }
+};
