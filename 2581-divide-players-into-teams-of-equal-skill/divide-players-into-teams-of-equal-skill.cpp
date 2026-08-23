e581f4c4c82fec3a1de1d71fea36f58ba4bf9ffe
Dhatrik Jaikritha <krithajai@gmail.com>
2026-08-23 17:36:25 +0530

    Time: 13 ms (65.43%) | Memory: 56.8 MB (20.80%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        unordered_map<int,int>mp;
+        int n=skill.size();
+        long long ans=0,sum=0;
+        for(int i=0;i<n;i++)sum+=skill[i];
+        if(sum%(n/2)!=0)return -1;
+        int target=sum/(n/2);
+        
+        for(int i=0;i<n;i++){
+           mp[skill[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            int temp=target-skill[i];
+            if(mp.find(temp)!=mp.end()){
+                ans+=(temp)*skill[i];
+                mp[temp]--;
+                if(!mp[temp])mp.erase(temp);
+                }
+            else return -1;
+        }
+        return ans/2;
+    }
+};
